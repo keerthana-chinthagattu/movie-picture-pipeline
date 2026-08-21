51c34ae0bd316db11aae165276781d4725f7315a
Keerthana Chinthagattu <chinthagattukeerthana30@gmail.com>
2026-08-21 15:05:18 +0530

    Fix EKS version and IAM policy

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -142,7 +142,7 @@ resource "aws_ecr_repository" "backend" {
 # Create an EKS cluster
 resource "aws_eks_cluster" "main" {
   name     = "cluster"
-  version  = var.k8s_version
+  version  = "1.31"
   role_arn = aws_iam_role.eks_cluster.arn
   vpc_config {
     subnet_ids              = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
@@ -316,15 +316,15 @@ resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
 }
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
+# resource "aws_iam_user_policy" "github_action_user_permission" {
+# user   = aws_iam_user.github_action_user.name
+# policy = data.aws_iam_policy_document.github_policy.json
+# }
 
-data "aws_iam_policy_document" "github_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ecr:*", "eks:*", "ec2:*", "iam:GetUser"]
-    resources = ["*"]
-  }
-}
+# data "aws_iam_policy_document" "github_policy" {
+# statement {
+#    effect    = "Allow"
+#    actions   = ["ecr:*", "eks:*", "ec2:*", "iam:GetUser"]
+#    resources = ["*"]
+#  }
+# }
